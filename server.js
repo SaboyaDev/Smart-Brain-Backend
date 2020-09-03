a8cc8201b9332f90a18625f546fe9f0fc5d5d19c
SaboyaDev <saboyadev@gmail.com>
2020-09-03 01:10:04 -0400

    fix db connection

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ app.use(express.json());
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
-		user: 'postgres',
-		password: '',
-		database: 'smart-brain',
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
 	},
 });
 
